c3291aed6f2bfe2e3116d46afda02a9d6de6699d
Eden <edenmassalonga@outlook.com>
2018-04-16 06:03:53 -0700

    Adicao de Configuracao de Banco de Dados

---
+++ src/main/java/com/aprendizado/webapp/models/Mensagem.java
@@ -0,0 +1,60 @@
+package com.aprendizado.webapp.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Mensagem implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long codigo;
+	
+	private String nome;
+	private String email;
+	private String mensagem;
+	
+	public Mensagem() {
+		
+	}	
+	
+	public long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	
+	
+}
